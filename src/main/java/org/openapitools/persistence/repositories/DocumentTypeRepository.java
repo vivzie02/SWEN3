0519fe81bccc0ee28fd3facd17785d4d12384302
TamimNi <tamim.aut@gmail.com>
2023-10-03 23:19:08 +0200

    repos created

---
+++ src/main/java/org/openapitools/persistence/repositories/DocumentTypeRepository.java
@@ -0,0 +1,6 @@
+package org.openapitools.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentTypeRepository extends JpaRepository<DocumentTypeEntity, Long> {
+}
---
+++ src/main/java/org/openapitools/persistence/repositories/UserInfoRepository.java
@@ -0,0 +1,5 @@
+package org.openapitools.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserInfoRepository extends JpaRepository<UserInfoEntity, Long>{
+}
