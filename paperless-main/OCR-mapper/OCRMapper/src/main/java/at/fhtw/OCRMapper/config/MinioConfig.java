45f8377cd990ebc82c882c088b20f3ffe7135ef5
vivzie02 <101602263+vivzie02@users.noreply.github.com>
2023-11-28 22:04:58 +0100

    Connection to minio works

---
+++ paperless-main/OCR-mapper/OCRMapper/src/main/java/at/fhtw/OCRMapper/config/MinioConfig.java
@@ -0,0 +1,19 @@
+package at.fhtw.OCRMapper.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                //für docker http://minio:9000
+                .endpoint("http://localhost:9000")
+                .credentials("minio-image", "minio-image-pass") // change as necessary
+                .region("at")
+                .build();
+    }
+}
--- paperless-main/OCR-mapper/OCRMapper/src/main/java/at/fhtw/OCRMapper/services/impl/OCRServiceImpl.java
+++ paperless-main/OCR-mapper/OCRMapper/src/main/java/at/fhtw/OCRMapper/services/impl/OCRServiceImpl.java
@@ -2,19 +2,53 @@ package at.fhtw.OCRMapper.services.impl;
 
 import at.fhtw.OCRMapper.OcrMapperApplication;
 import at.fhtw.OCRMapper.services.OCRService;
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
+import io.minio.messages.Item;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 @Service
 public class OCRServiceImpl implements OCRService {
     static Logger log = Logger.getLogger(OCRServiceImpl.class.getName());
+
+    @Autowired
+    private MinioClient minioClient;
+
     @Override
     @RabbitListener(queues = "paperless.documents.queue")
     public void saveDocument(String message){
         if(!message.equals("")) {
             log.info("successfully read info from queue");
+            log.info(message);
+        }
+
+        getFile("mein-bucket", "object-name/Review_Mindmap_Ziering.pdf");
+    }
+
+    public InputStream getFile(String bucketName, String objectName){
+        try {
+            InputStream client = minioClient.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .build()
+            );
+            log.info("successfully read file from minio");
+            return client;
+
+        } catch (MinioException | IOException | InvalidKeyException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            // Handle exceptions appropriately
+            return null;
         }
     }
 }
